17d77564d3bc9e2b9b512a941de24f68a86ecc1c
Yeshasvitvs <yeshasvitvs@gmail.com>
2018-01-22 14:46:33 +0100

    Update robots gain

--- controllers/fixedEEControl/app/robots/iCubGenova02/gains.m
+++ controllers/fixedEEControl/app/robots/iCubGenova02/gains.m
@@ -8,17 +8,17 @@ end
 ROBOT_DOF_FOR_SIMULINK                      = eye(ROBOT_DOF);
 
 %% Position Control Gains
-GAINS.POSITION.Kp			    = 1000;
-GAINS.POSITION.Kd			    = 2*sqrt(GAINS.POSITION.Kp);
-GAINS.POSITION.Eps			    = 1e-20;
+GAINS.POSITION.Kp			    = 200;
+GAINS.POSITION.Kd			    = 0*2*sqrt(GAINS.POSITION.Kp);
+GAINS.POSITION.Eps			    = 1e-7;
 
 %% Orientation Control Gains
 GAINS.ORIENTATION.Kp			    = 50;
-GAINS.ORIENTATION.Kd			    = 2*sqrt(GAINS.ORIENTATION.Kp);
-GAINS.ORIENTATION.Eps			    = 1e-20;
+GAINS.ORIENTATION.Kd			    = 0*2*sqrt(GAINS.ORIENTATION.Kp);
+GAINS.ORIENTATION.Eps			    = 1e-7;
 
 %% Postural Task Gains
-GAINS.POSTURAL.Kp			    = diag([100,100,100,30,30,30,30,30,100,100,100,100,100]);
+GAINS.POSTURAL.Kp			    = diag([30,30,30,5,5,5,5,5,5,5,5,5,5]);
 GAINS.POSTURAL.Kd			    = 2;
 
 
