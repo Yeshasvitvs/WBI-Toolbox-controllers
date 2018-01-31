6a3ea47a6a2a22acacc1569c4899a3502a9d8e27
Yeshasvitvs <yeshasvitvs@gmail.com>
2018-01-31 15:36:10 +0100

    Add human interrupt home position

--- controllers/pHRIControl/two_icubs_spawn.sh
+++ controllers/pHRIControl/two_icubs_spawn.sh
@@ -4,5 +4,5 @@ gz model -m iCub -f /home/yeshi/SOFTWARE/icub-gazebo/icub/icub.sdf --pose-z 1 --
 
 sleep 2
 
-gz model -m iCub_0 -f /home/yeshi/SOFTWARE/icub-gazebo/icub/icub.sdf --pose-x 0.8 --pose-z 1 
+gz model -m iCub_0 -f /home/yeshi/SOFTWARE/icub-gazebo/icub/icub.sdf --pose-x 0.7 --pose-z 1 
 
