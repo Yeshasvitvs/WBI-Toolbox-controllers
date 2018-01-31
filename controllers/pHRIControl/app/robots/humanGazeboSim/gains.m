f6f18d55b620ab3687139e598eb890be5a8cbf20
Yeshasvitvs <yeshasvitvs@gmail.com>
2018-01-31 15:41:28 +0100

    Minor fix

--- controllers/pHRIControl/app/robots/humanGazeboSim/gains.m
+++ controllers/pHRIControl/app/robots/humanGazeboSim/gains.m
@@ -10,7 +10,7 @@ HUMAN.ROBOT_DOF_FOR_SIMULINK                      = eye(HUMAN.ROBOT_DOF);
 %% Position Control Gains
 HUMAN.GAINS.POSITION.Kp			    = diag([1000,1000,1000]);
 HUMAN.GAINS.POSITION.Kd			    = 2*sqrt(HUMAN.GAINS.POSITION.Kp);
-HUMAN.GAINS.POSITION.Eps			    = 1e-20;
+HUMAN.GAINS.POSITION.Eps            = 1e-20;
 
 %% Postural Task Gains
 HUMAN.GAINS.POSTURAL.Kp			    = diag([100,100,100,30,30,30,30,100,100,100,100]);
